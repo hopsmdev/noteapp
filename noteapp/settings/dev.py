02a73add5e16a72f75fac66abd8bb593d25b8404
hopsmdev <hopsmdev@gmail.com>
2016-06-28 23:09:43 +0200

    add mongodb to dev env - connection is sucessful

--- noteapp/settings/dev.py
+++ noteapp/settings/dev.py
@@ -1,3 +1,25 @@
+import mongoengine
 from .base import *
 
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+    },
+}
+
+SESSION_ENGINE = 'mongoengine.django.sessions'
+
+MONGODB_USER = os.environ.get("MONGODB_USER")
+MONGODB_PASSWD = os.environ.get("MONGODB_PASSWD")
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
+MONGODB_NAME = os.environ.get("MONGODB_NAME")
+
+mongoengine.connect(MONGODB_NAME,
+                    host=('mongodb://{MONGODB_USER}:{MONGODB_PASSWD}@'
+                          '{MONGODB_HOST}/{MONGODB_NAME}'.format(**locals())))
+
+AUTHENTICATION_BACKENDS = (
+    'mongoengine.django.auth.MongoEngineBackend',
+)
