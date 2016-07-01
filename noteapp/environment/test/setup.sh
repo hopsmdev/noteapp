68c058c271ff164361459e602e8e24142f432d9d
hopsmdev <hopsmdev@gmail.com>
2016-07-01 22:47:30 +0200

    add environment dir, docker-compose and setup env script

---
+++ noteapp/environment/test/setup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+export MONGODB_USER='test'
+export MONGODB_PASSWORD='test'
+export MONGODB_PORT=27017
+export MONGODB_NAME='test'
+
+docker-compose -f $(dirname "${0}")/docker-compose.yml up --force-recreate -d
+
+docker exec test_mongodb_1 mongo $MONGODB_NAME --eval "db.dropAllUsers();
+    db.createUser({ user: '$MONGODB_USER', pwd: '$MONGODB_PASSWORD', \
+    roles: [ { role: 'dbOwner', db: '$MONGODB_NAME' } ] });"
