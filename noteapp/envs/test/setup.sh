458238c2bca7552e43cc7b29f668633c1f4f1cb1
hopsmdev <hopsmdev@gmail.com>
2016-07-02 20:06:35 +0200

    remove --force-recreate flag

--- noteapp/envs/test/setup.sh
+++ noteapp/envs/test/setup.sh
@@ -4,7 +4,7 @@ export MONGODB_PASSWORD='test'
 export MONGODB_PORT=27017
 export MONGODB_NAME='testdb'
 
-docker-compose -f $(dirname "${0}")/docker-compose.yml up --force-recreate -d
+docker-compose -f $(dirname "${0}")/docker-compose.yml up -d
 
 docker exec test_mongodb_1 mongo $MONGODB_NAME --eval "db.dropAllUsers();
     db.createUser({ user: '$MONGODB_USER', pwd: '$MONGODB_PASSWORD', \
