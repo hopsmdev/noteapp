60a77b80db1dc364ae95e05a86232471e352c30a
hopsmdev <hopsmdev@gmail.com>
2016-07-03 14:06:59 +0200

    update model test

--- notes/models.py
+++ notes/models.py
@@ -1,19 +1,17 @@
 import datetime
-from django.core.urlresolvers import reverse
 from django.utils.text import slugify
 from bson.objectid import ObjectId
-from mongoengine import Document, EmbeddedDocument, fields, ObjectIdField
+from mongoengine import Document, EmbeddedDocument, PULL
+from mongoengine.fields import *
 
 
 class Comment(EmbeddedDocument):
     _id = ObjectIdField(
         required=True, default=lambda: ObjectId(), primary_key=True)
-    pub_date = fields.DateTimeField(
-        default=datetime.datetime.now, required=True)
-    author = fields.StringField(
-        verbose_name="Author", max_length=255, required=True)
-    email = fields.EmailField(verbose_name="Email")
-    text = fields.StringField(verbose_name="Comment", required=True)
+    pub_date = DateTimeField(default=datetime.datetime.now, required=True)
+    author = StringField(verbose_name="Author", max_length=255, required=True)
+    email = EmailField(verbose_name="Email")
+    text = StringField(verbose_name="Comment", required=True)
 
     def short_text(self):
         return ' '.join(self.text.split()[:20]) + " ..."  \
@@ -24,14 +22,22 @@ class Comment(EmbeddedDocument):
             self.author, self.pub_date.strftime('%Y-%m-%d'))
 
 
+class Tag(Document):
+
+    tag = StringField(max_length=128, required=True,
+                      primary_key=True, unique=True)
+
+    def __str__(self):
+        return str(self.tag)
+
+
 class Note(Document):
-    pub_date = fields.DateTimeField(
-        default=datetime.datetime.now, required=True)
-    title = fields.StringField(max_length=255, required=True)
-    slug = fields.StringField(max_length=255, required=True, primary_key=True)
-    comments = fields.ListField(fields.EmbeddedDocumentField('Comment'))
-    text = fields.StringField(verbose_name="Text", required=True)
-    tags = fields.ListField()
+    pub_date = DateTimeField(default=datetime.datetime.now, required=True)
+    title = StringField(max_length=255, required=True)
+    slug = StringField(max_length=255, required=True)
+    text = StringField(verbose_name="Text", required=True)
+    comments = ListField(EmbeddedDocumentField('Comment'))
+    tags = ListField(ReferenceField(Tag, reverse_delete_rule=PULL))
 
     meta = {
         'allow_inheritance': True,
@@ -43,9 +49,6 @@ class Note(Document):
         if not self.slug:
             self.slug = slugify(self.title)[:50]
         return super(Note, self).save(*args, **kwargs)
-
-    def get_absolute_url(self):
-        return reverse('note', kwargs={"slug": self.slug})
 
     def formatted_title(self):
         return self.title.title()
@@ -60,3 +63,36 @@ class Note(Document):
     def __str__(self):
         return "{} on {}".format(
             self.title, self.pub_date.strftime('%Y-%m-%d'))
+
+    def delete_tag(self, tag_name=None):
+
+        tag = Tag.objects(tag=tag_name).first()
+        if not tag:
+            print("Tag {} does not exist, cannot remove".format(tag_name))
+            return
+
+        Note.objects(id=self.id).update_one(
+            pull__tags=Tag.objects(tag=tag_name).first())
+        self.save()
+
+    def delete_comment(self, comment_id=None):
+        Note.objects(id=self.id).update_one(
+            pull__comments={'_id': comment_id})
+        self.save()
+
+    def add_comment(self, comment):
+        if comment:
+            Note.objects(id=self.id).update_one(push__comments=comment)
+            self.save()
+
+    def add_tag(self, tag_name=None):
+
+        tag = Tag.objects(tag=tag_name).first()
+        if not tag:
+            print("Tag {} does not exist, create new one".format(tag_name))
+            tag = Tag(tag=tag_name)
+            tag.save()
+
+        tag = Tag.objects(tag=tag_name).first()
+        Note.objects(id=self.id).update_one(push__tags=tag)
+        self.save()
--- notes/tests.py
+++ notes/tests.py
@@ -19,25 +19,39 @@ class NoteModelTest(TestCase):
 
     def setUp(self):
 
-        self.comment_auth1_1 = Comment(
-            author="Author1",
-            email="author1@test.com",
-            text="First comment of Author1")
+        self.tag_a = Tag(tag='a')
+        self.tag_b = Tag(tag='b')
+        self.tag_c = Tag(tag='c')
 
-        self.comment_auth1_2 = Comment(
-            author="Author1",
-            email="author1@test.com",
-            text="Second comment of Author1")
+        self.tag_a.save()
+        self.tag_b.save()
+        self.tag_c.save()
 
-        self.comment_auth2_1 = Comment(
-            author="Author2",
-            email="author2@test.com",
-            text="First comment of Author2")
+        comment_1 = {"author": "Author1",
+                     "email": "author1@test.com",
+                     "text": "First comment of Author1"}
+
+        comment_2 = {"author": "Author1",
+                     "email": "author1@test.com",
+                     "text": "Second comment of Author1"}
+
+        comment_3 = {"author": "Author2",
+                     "email": "author2@test.com",
+                     "text": "First comment of Author2"}
+
+        comment_4 = {"author": "Author2",
+                     "email": "author2@test.com",
+                     "text": "Second comment of Author2"}
+
+        self.comment_auth1_1 = Comment(**comment_1)
+        self.comment_auth1_2 = Comment(**comment_2)
+        self.comment_auth2_1 = Comment(**comment_3)
+        self.comment_auth2_2 = Comment(**comment_4)
 
         self.short_note = Note(
             title="test note",
             text="test note text",
-            tags=['a', 'b'],
+            tags=[self.tag_a, self.tag_b],
             slug="short-note",
             comments=[
                 self.comment_auth1_1,
@@ -47,7 +61,7 @@ class NoteModelTest(TestCase):
         self.long_note = Note(
             title="test long note",
             text="".join(str(i) for i in range(0, 100)),
-            tags=['a', 'b', 'c'],
+            tags=[self.tag_a, self.tag_b, self.tag_c],
             comments=[
                 self.comment_auth1_1,
                 self.comment_auth1_2,
@@ -56,9 +70,10 @@ class NoteModelTest(TestCase):
         self.note_noslug = Note(
             title="test noslug note",
             text="test noslug note text",
-            tags=['a', 'b'])
+            tags=[self.tag_a, self.tag_b],)
 
     def test_string_repr(self):
+
         self.assertEqual(
             str(self.short_note),
             "{} on {}".format(
@@ -66,20 +81,23 @@ class NoteModelTest(TestCase):
                 self.short_note.pub_date.strftime('%Y-%m-%d')))
 
     def test_save_notes(self):
+
         self.short_note.save()
         note = Note.objects(slug="short-note").first()
         self.assertEqual(note.slug, 'short-note')
 
     def test_note_noslug(self):
+
         correct_slug = "test-noslug-note"
         self.note_noslug.save()
         self.assertEqual(self.note_noslug.get_slug(), correct_slug)
-        self.assertEqual(self.note_noslug.id, correct_slug)
+        self.assertEqual(self.note_noslug.slug, correct_slug)
 
         note = Note.objects(title__contains="noslug").first()
         self.assertEqual(note.slug, correct_slug)
 
     def test_tags_exists(self):
+
         self.short_note.save()
         self.long_note.save()
         self.note_noslug.save()
@@ -91,10 +109,12 @@ class NoteModelTest(TestCase):
         self.assertEqual(len(notes), 1)
 
     def test_tags_notexist(self):
+
         notes = Note.objects(tags__in='x')
         self.assertEqual(len(notes), 0)
 
     def test_get_Author1_comments(self):
+
         self.short_note.save()
         self.long_note.save()
         self.note_noslug.save()
@@ -107,6 +127,7 @@ class NoteModelTest(TestCase):
         self.assertEqual(str(list(set(comments))[0]), "author1@test.com")
 
     def test_delete_note(self):
+
         self.short_note.save()
         self.long_note.save()
         self.note_noslug.save()
@@ -118,31 +139,123 @@ class NoteModelTest(TestCase):
         notes = Note.objects()
         self.assertEqual(len(notes), 2)
 
-    def test_delete_note_comment(self):
+    def test_delete_comment_from_note(self):
 
         self.short_note.save()
-        print(self.comment_auth1_1)
         note = Note.objects(slug="short-note").first()
         comment_ids = [comment._id for comment in note.comments]
         self.assertEqual(len(comment_ids), 3)
 
         comment_to_remove = comment_ids[0]
-        print(comment_to_remove, type(comment_to_remove))
 
         Note.objects(slug="short-note").update_one(
             pull__comments={'_id': comment_to_remove})
 
         note.reload()
         comment_ids = [comment._id for comment in note.comments]
-        print(comment_ids)
+        self.assertEqual(len(comment_ids), 2)
+
+    def test_delete_note_comment(self):
+
+        self.short_note.save()
+        note = Note.objects(slug="short-note").first()
+        comment_ids = [comment._id for comment in note.comments]
+        self.assertEqual(len(comment_ids), 3)
+        comment_to_remove = comment_ids[0]
+
+        self.short_note.delete_comment(comment_to_remove)
+
+        note.reload()
+        comment_ids = [comment._id for comment in note.comments]
+        self.assertEqual(len(comment_ids), 2)
 
     def test_delete_tag_from_note(self):
-        pass
 
+        self.short_note.save()
+        note = Note.objects(slug="short-note").first()
+        note_tags = [tag.tag for tag in note.tags]
+        self.assertEqual(len(note_tags), 2)
 
-    def test_append_comment(self):
-        pass
+        tag_to_remove = self.tag_a
 
+        Note.objects(slug="short-note").update_one(
+            pull__tags=tag_to_remove)
 
-    def test_append_tag(self):
-        pass
+        note.reload()
+        note_tags = [tag.tag for tag in note.tags]
+        self.assertEqual(len(note_tags), 1)
+
+        # confirm that we only unset tag from note - we do not want to remove
+        # tag from db
+        self.assertTrue(Tag.objects(tag='a'))
+
+    def test_delete_tag_from_note_using_tag_name(self):
+
+        self.short_note.save()
+        self.short_note.delete_tag(tag_name='a')
+        self.short_note.reload()
+        note_tags = [tag.tag for tag in self.short_note.tags]
+        self.assertEqual(len(note_tags), 1)
+
+    def test_delete_notexisting_tag_from_note(self):
+
+        self.short_note.save()
+        self.short_note.delete_tag(tag_name='XX')
+        self.short_note.reload()
+        note_tags = [tag.tag for tag in self.short_note.tags]
+        self.assertEqual(len(note_tags), 2)
+
+    def test_delete_tag(self):
+
+        self.short_note.save()  # note taged with 2 tags: a and b
+        note = Note.objects(slug='short-note').first()
+        self.assertEqual(len(note.tags), 2)
+
+        tags = Tag.objects()
+        self.assertEqual(len(tags), 3)
+        self.tag_a.delete()
+
+        tags = Tag.objects()
+        self.assertEqual(len(tags), 2)
+
+        # confirm that tag was also removed from Note
+
+        note = Note.objects(slug='short-note').first()
+        self.assertEqual(len(note.tags), 1)
+
+    def test_add_comment(self):
+
+        self.short_note.save()
+        note = Note.objects(slug="short-note").first()
+        self.assertEqual(len(note.comments), 3)
+
+        self.short_note.add_comment(self.comment_auth2_2)
+
+        note.reload()
+        self.assertEqual(len(note.comments), 4)
+
+    def test_add_tag_to_note(self):
+
+        new_tag = "c"
+
+        self.short_note.save()
+        self.short_note.add_tag(tag_name=new_tag)
+        self.short_note.reload()
+        note_tags = [tag.tag for tag in self.short_note.tags]
+        self.assertEqual(len(note_tags), 3)
+
+        tag = Tag.objects(tag=new_tag).first()
+        self.assertEqual(tag.tag, new_tag)
+
+    def test_add_notexisting_tag_to_note(self):
+
+        new_tag = "NEWTAG"
+
+        self.short_note.save()
+        self.short_note.add_tag(tag_name=new_tag)
+        self.short_note.reload()
+        note_tags = [tag.tag for tag in self.short_note.tags]
+        self.assertEqual(len(note_tags), 3)
+
+        tag = Tag.objects(tag=new_tag).first()
+        self.assertEqual(tag.tag, new_tag)
