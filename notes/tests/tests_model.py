88aada6f3de6b99e2245f2fa443840cdfe703048
hopsmdev <hopsmdev@gmail.com>
2016-07-05 23:09:04 +0200

    update test cases for model

--- notes/tests/tests_model.py
+++ notes/tests/tests_model.py
@@ -77,49 +77,13 @@ class NoteModelTest(TestCaseMongo, TestDataFactory):
 
     def test_delete_note_comment(self):
 
-        note = Note.objects(slug="short-note").first()
-        comment_ids = [comment._id for comment in note.comments]
-        self.assertEqual(len(comment_ids), 3)
-        comment_to_remove = comment_ids[0]
-
-        self.short_note.delete_comment(comment_to_remove)
-
-        note.reload()
-        comment_ids = [comment._id for comment in note.comments]
-        self.assertEqual(len(comment_ids), 2)
+        self.short_note.remove(comments=[self.comment_auth1_1])
+        self.assertEqual(len(self.short_note.comments), 2)
 
     def test_delete_tag_from_note(self):
 
-        note = Note.objects(slug="short-note").first()
-        note_tags = [tag.tag for tag in note.tags]
-        self.assertEqual(len(note_tags), 2)
-
-        tag_to_remove = self.tag_a
-
-        Note.objects(slug="short-note").update_one(
-            pull__tags=tag_to_remove)
-
-        note.reload()
-        note_tags = [tag.tag for tag in note.tags]
-        self.assertEqual(len(note_tags), 1)
-
-        # confirm that we only unset tag from note - we do not want to remove
-        # tag from db
-        self.assertTrue(Tag.objects(tag='a'))
-
-    def test_delete_tag_from_note_using_tag_name(self):
-
-        self.short_note.delete_tag(tag_name='a')
-        self.short_note.reload()
-        note_tags = [tag.tag for tag in self.short_note.tags]
-        self.assertEqual(len(note_tags), 1)
-
-    def test_delete_notexisting_tag_from_note(self):
-
-        self.short_note.delete_tag(tag_name='XX')
-        self.short_note.reload()
-        note_tags = [tag.tag for tag in self.short_note.tags]
-        self.assertEqual(len(note_tags), 2)
+        self.short_note.remove(tags=[self.tag_a])
+        self.assertEqual(len(self.short_note.tags), 1)
 
     def test_delete_tag(self):
 
@@ -143,34 +107,47 @@ class NoteModelTest(TestCaseMongo, TestDataFactory):
         note = Note.objects(slug="short-note").first()
         self.assertEqual(len(note.comments), 3)
 
-        self.short_note.add_comment(self.comment_auth2_2)
+        self.short_note.update(comments=[self.comment_auth2_2])
 
         note.reload()
         self.assertEqual(len(note.comments), 4)
 
     def test_add_tag_to_note(self):
 
-        new_tag = "c"
-
-        self.short_note.add_tag(tag_name=new_tag)
+        self.short_note.update(tags=[self.tag_c])
         self.short_note.reload()
         note_tags = [tag.tag for tag in self.short_note.tags]
+
         self.assertEqual(len(note_tags), 3)
 
-        tag = Tag.objects(tag=new_tag).first()
-        self.assertEqual(tag.tag, new_tag)
+        tag = Tag.objects(tag=self.tag_c.tag).first()
+        self.assertEqual(tag.tag, self.tag_c.tag)
+
+    def test_add_the_same_tag_multiple_times_to_note(self):
+
+        self.short_note.update(tags=[self.tag_c])
+        self.short_note.update(tags=[self.tag_c])
+        self.short_note.reload()
+        note_tags = [tag.tag for tag in self.short_note.tags]
+
+        self.assertEqual(len(note_tags), 3)
+
+        tag = Tag.objects(tag=self.tag_c.tag).first()
+        self.assertEqual(tag.tag, self.tag_c.tag)
+
 
     def test_add_notexisting_tag_to_note(self):
 
-        new_tag = "NEWTAG"
+        new_tag = Tag(tag='d')
 
-        self.short_note.add_tag(tag_name=new_tag)
+        self.short_note.update(tags=[new_tag])
         self.short_note.reload()
         note_tags = [tag.tag for tag in self.short_note.tags]
+
         self.assertEqual(len(note_tags), 3)
 
-        tag = Tag.objects(tag=new_tag).first()
-        self.assertEqual(tag.tag, new_tag)
+        tag = Tag.objects(tag=new_tag.tag).first()
+        self.assertEqual(tag.tag, new_tag.tag)
 
     def test_edit_comment(self):
         pass
@@ -178,5 +155,20 @@ class NoteModelTest(TestCaseMongo, TestDataFactory):
     def test_edit_note(self):
         pass
 
-    def test_edit_note_title(self):
-        pass
+    def test_update_note_title(self):
+
+        self.short_note.update(title="test")
+        self.short_note.reload()
+        self.assertEqual(self.short_note.title, 'test')
+
+    def test_update_note(self):
+
+        self.short_note.update(text="test")
+        self.short_note.reload()
+        self.assertEqual(self.short_note.text, 'test')
+
+    def test_update_nonfield(self):
+
+        self.short_note.update(doesnotexist="doesnotexist")
+        self.short_note.reload()
+
