7a8404666884785d13906da5fc058552feb324f5
hopsmdev <hopsmdev@gmail.com>
2016-07-05 23:08:47 +0200

    add create method

--- notes/api/serializers.py
+++ notes/api/serializers.py
@@ -28,24 +28,18 @@ class NoteSerializer(DocumentSerializer):
 
     def create(self, validated_data):
 
-        print("#####################", validated_data)
         comments = validated_data.pop('comments')
         tags = validated_data.pop('tags')
 
         new_note = Note(**validated_data)
-        new_note.save()
 
-        for comment_data in comments:
-            new_note.comments.append(Comment(**comment_data))
-
-        print("----", tags)
-        for tag_data in tags:
-            print(tag_data['tag'])
-            new_note.add_tag(tag_name=tag_data['tag'])
-            #new_note.tags.append(Tag(**tag_data))
+        new_note.tags.extend([(Tag(**tag_data)) for tag_data in tags])
+        new_note.comments.extend([(Comment(**comment_data))
+                                  for comment_data in comments])
 
         new_note.save()
         return new_note
+
 
 
 
